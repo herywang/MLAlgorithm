e744307e524b84eb0d815920781bd8391cbb039a
wangheng <673602165@qq.com>
2018-11-26 10:20:43 +0800

    update binary tree

--- tree/Tree.py
+++ tree/Tree.py
@@ -53,6 +53,7 @@ class Solution:
 
     '''
     二叉树的顺序存储
+    根节点position: n, 左子树：2*n+1, 右子树：2*n+2
     '''
     def tree2array(self, root, len):
         self.array = [None] * len
@@ -64,7 +65,54 @@ class Solution:
         self.array[pos] = node.value
         self.__toArrar(node.left, 2*pos + 1)
         self.__toArrar(node.right, 2*pos + 2)
-            
+
+    '''
+    二叉树最小深度
+    '''
+    def min_depth(self, node):
+        if node is None:
+            return 0
+        if node.left is not None:
+            if node.right is not None:
+                return self.__min(self.min_depth(node.left), self.min_depth(node.right)) + 1
+            else:
+                return self.min_depth(node.left)
+        elif node.right is not None:
+            return self.min_depth(node.right) + 1
+        else:
+            return 1
+
+    def __min(self, a, b):
+        if a>=b:
+            return b
+        else:
+            return a
+
+    '''
+    二叉树最大深度
+    '''
+    def max_depth(self, node):
+        if node is None:
+            return 0
+        else:
+            return self.__max(self.max_depth(node.left), self.max_depth(node.right)) + 1
+
+    def __max(self, a, b):
+        if a >= b:
+            return a
+        else:
+            return b
+    
+    '''
+    判断二叉树是否相同
+    '''
+    def isSampleTree(self, treeA, treeB):
+        if treeA is None:
+            return treeB is None
+        if treeB is None:
+            return False
+        return (treeA.value == treeB.value) and self.isSampleTree(treeA.left, treeB.left) \
+                and self.isSampleTree(treeA.right, treeB.right)
 
 
 
@@ -104,3 +152,12 @@ if __name__ == '__main__':
     print('二叉树的顺序存储：')
     solution.tree2array(tree, 16)
     print(solution.array)
+    print("二叉树最小深度：")
+    print(solution.min_depth(tree))
+    print("二叉树最大深度：")
+    print(solution.max_depth(tree))
+    print("判断二叉树是否相等")
+    treeB = TreeNode('D', \
+        TreeNode('B', TreeNode('A'), TreeNode('C')), \
+        TreeNode('E', TreeNode('G', right=TreeNode('F'))))
+    print(solution.isSampleTree(tree, treeB))
