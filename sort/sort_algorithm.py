918004fbdca4df8c76c9870a5a95759058d624fa
wangheng <673602165@qq.com>
2018-11-27 18:37:43 +0800

    双轴快速排序修正

--- sort/sort_algorithm.py
+++ sort/sort_algorithm.py
@@ -5,24 +5,28 @@ class Sort:
         pass
     
     def quick_sort(self, array, left, right):
-        i = left
+        i = left + 1
         j = right
-        tmp = array[(left + right) // 2]
+        tmp = array[left]
         while i<=j:
-            while i<j and array[i] <= tmp:
+            while i<=j and array[i] <= tmp:
                 i += 1
-            while i<j and array[j] >= tmp:
+            while i<=j and array[j] >= tmp:
                 j -= 1
-            if i <= j:
-                t = array[i]
-                array[i] = array[j]
-                array[j] = t
+            if i < j:
+                self.__swap(array, i, j)
                 i += 1
                 j -= 1
+        self.__swap(array, left, j)
         if left < j:
-            self.quick_sort(array, left, j)
+            self.quick_sort(array, left, j-1)
         if i < right:
-            self.quick_sort(array, i, right)
+            self.quick_sort(array, j+1, right)
+
+    def __swap(self, arr, a, b):
+        tmp = arr[a]
+        arr[a] = arr[b]
+        arr[b] = tmp
             
 if __name__ == "__main__":
     arr = [3, 4, 5, 1, 2, 4, 5, 2, 23, 21, 0]
