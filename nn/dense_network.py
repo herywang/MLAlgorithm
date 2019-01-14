fe43c5d6444ed7f3a3a6f3824fef4a774655a5a4
wangheng <673602165@qq.com>
2019-01-14 15:24:29 +0800

    linux push

--- nn/dense_network.py
+++ nn/dense_network.py
@@ -64,26 +64,3 @@ class NeuralNetwork:
 
     def train(self, model=TRAIN_MODEL_NEW, ckpt_file='work/dae.ckpt'):
         pass
-=======
-#实现仿射传播
-from __future__ import absolute_import, division, print_function
-
-import numpy as np
-
-class NeuralNetwork:
-    def __init__(self):
-        pass
-
-
-    @staticmethod
-    def affine_forward(x, w, b):
-        """
-        :param x: shape:[N, D]
-        :param w: shape:[D, M]
-        :param b: shape:[M]
-        :return: out: shape[N, M], cache: shape(x, w, b)
-        """
-        out = np.dot(x, w) + b
-        cache = (x, w, b)
-        return out, cache
->>>>>>> b986d7aaae6c07f2b0775cddd148bd0b13e7e78b
